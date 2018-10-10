f5b2a1babd3c41d7f67ac90ea2d8389d6f1a48f6
Cybertinus <github@cybertinus.nl>
2018-10-10 13:45:19 +0200

    Upload the mac address extractor

---
+++ mac.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+##########
+# CONFIG #
+##########
+
+ouilocation="/home/user/oui.txt"
+
+#################
+# ACTUAL SCRIPT #
+#################
+
+if [ ! -f "${ouilocation}" ] ; then
+	echo 'OUI location can not be found. Have you updated the config?' 1>&2
+	exit 1
+fi
+
+if [ -z "${1}" ] ; then
+	echo 'No mac address specified, please do so as first argument' 1>&2
+	exit 2
+fi
+
+macaddress="$(echo "${1}" | sed -e 's/[-.:]//g' | tr '[:lower:]' '[:upper:]')"
+macaddress="${macaddress:0:6}"
+echo -n "${1} = "
+
+vendor="$(awk "/^${macaddress}/ {print substr(\$0,index(\$0,\$4))}" "${ouilocation}")"
+if [ -z "${vendor}" ] ; then
+	echo "Not specified in ${ouilocation}"
+else
+	echo "${vendor}"
+fi
